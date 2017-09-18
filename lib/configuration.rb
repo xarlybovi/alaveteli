38fb55b328004c1748a0090369cdcbb725237b0d
Louise Crow <louise.crow@gmail.com>
2017-09-18 13:22:55 +0100

    Remove VARNISH_HOST param
    Also remove ACCELERATOR_HOST and ACCELERATOR_PORT from test.yml.
    Looks like these were related obsolete params.

--- lib/configuration.rb
+++ lib/configuration.rb
@@ -113,7 +113,6 @@ module AlaveteliConfiguration
       :USE_GHOSTSCRIPT_COMPRESSION => false,
       :USE_MAILCATCHER_IN_DEVELOPMENT => true,
       :UTILITY_SEARCH_PATH => ["/usr/bin", "/usr/local/bin"],
-      :VARNISH_HOST => '',
       :WORKING_OR_CALENDAR_DAYS => 'working',
       :USE_BULLET_IN_DEVELOPMENT => false,
       :EXTERNAL_REVIEWERS => ''
